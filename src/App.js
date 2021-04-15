bdb01b76d0ef67c7ee74d93ec6b00d8e34ae0fc8
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-04-15 10:28:16 -0600

    second test

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import { useState } from "react";
 
 function App() {
   const [btnColor, setColor] = useState("red");
+  const [disabled, setDisable] = useState(false);
 
   const setNewColor = () => {
     setColor(btnColor === "red" ? "blue" : "red");
+  };
+
+  const disableBtn = () => {
+    setDisable(!disabled);
   };
 
   return (
@@ -14,9 +19,17 @@ function App() {
         style={{ backgroundColor: btnColor }}
         onClick={setNewColor}
         className="btn"
+        disabled={disabled}
       >
         Change to blue
       </button>
+
+      <input
+        type="checkbox"
+        name="checkbox"
+        id="checkbox"
+        onChange={disableBtn}
+      />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,29 +1,41 @@
-import {fireEvent, render, screen} from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
 beforeEach(() => {
   render(<App />);
-
-})
-
-it('should have correct initial color', function () {
-
-const btn = screen.getByRole('button', {name: /change to blue/i})
-
-  expect(btn).toHaveStyle({ backgroundColor: 'red' });
-
-  fireEvent.click(btn)
-
-  expect(btn).toHaveStyle({ backgroundColor: 'blue' });
-  expect(btn.textContent).toBe('Change to blue')
-
-})
-
-it('should have correct initial text', function () {
-  const btn = screen.getByRole('button', {name: /change to blue/i})
-
-
-
 });
 
+it("should have correct initial color", function () {
+  const btn = screen.getByRole("button", { name: /change to blue/i });
 
+  expect(btn).toHaveStyle({ backgroundColor: "red" });
+
+  fireEvent.click(btn);
+
+  expect(btn).toHaveStyle({ backgroundColor: "blue" });
+  expect(btn.textContent).toBe("Change to blue");
+});
+
+it("should button be enabled", function () {
+  const btn = screen.getByRole("button", { name: /change to blue/i });
+  expect(btn).toBeEnabled();
+
+  const checkBox = screen.getByRole("checkbox");
+  expect(checkBox).not.toBeChecked();
+});
+
+it("should toggle button enabled/disabled", function () {
+  const btn = screen.getByRole("button", { name: /change to blue/i });
+  expect(btn).toBeEnabled();
+
+  const checkBox = screen.getByRole("checkbox");
+  expect(checkBox).not.toBeChecked();
+
+  fireEvent.click(checkBox);
+
+  expect(btn).toBeDisabled();
+
+  fireEvent.click(checkBox);
+
+  expect(btn).toBeEnabled();
+});

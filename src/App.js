c18773b39e1705743ea9e112da308efa68455d57
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-04-15 12:29:48 -0600

    eslint setup

--- src/App.js
+++ src/App.js
@@ -5,37 +5,39 @@ export function replaceCamelCase(colorName) {
   return colorName.replace(/\B([A-Z])\B/g, " $1");
 }
 
-function App() {
-  const [btnColor, setColor] = useState("red");
-  const [disabled, setDisable] = useState(false);
+// function App() {
+//   const [btnColor, setColor] = useState("red");
+//   const [disabled, setDisable] = useState(false);
+//
+//   const setNewColor = () => {
+//     setColor(btnColor === "red" ? "blue" : "red");
+//   };
+//
+//   const disableBtn = () => {
+//     setDisable(!disabled);
+//   };
+//
+//   return (
+//     <div className="App">
+//       <button
+//         style={{ backgroundColor: disabled ? "grey" : btnColor }}
+//         onClick={setNewColor}
+//         className="btn"
+//         disabled={disabled}
+//       >
+//         Change to blue
+//       </button>
+//       <label htmlFor="checkbox">disabled button</label>
+//       <input
+//         type="checkbox"
+//         name="disabled button"
+//         id="checkbox"
+//         onChange={disableBtn}
+//       />
+//     </div>
+//   );
+// }
 
-  const setNewColor = () => {
-    setColor(btnColor === "red" ? "blue" : "red");
-  };
-
-  const disableBtn = () => {
-    setDisable(!disabled);
-  };
-
-  return (
-    <div className="App">
-      <button
-        style={{ backgroundColor: disabled ? "grey" : btnColor }}
-        onClick={setNewColor}
-        className="btn"
-        disabled={disabled}
-      >
-        Change to blue
-      </button>
-      <label htmlFor="checkbox">disabled button</label>
-      <input
-        type="checkbox"
-        name="disabled button"
-        id="checkbox"
-        onChange={disableBtn}
-      />
-    </div>
-  );
-}
+function App() {}
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -6,64 +6,64 @@ beforeEach(() => {
   render(<App />);
 });
 
-it("should have correct initial color", function () {
-  const btn = screen.getByRole("button", { name: /change to blue/i });
-
-  expect(btn).toHaveStyle({ backgroundColor: "red" });
-
-  fireEvent.click(btn);
-
-  expect(btn).toHaveStyle({ backgroundColor: "blue" });
-  expect(btn.textContent).toBe("Change to blue");
-});
-
-it("should button be enabled", function () {
-  const btn = screen.getByRole("button", { name: /change to blue/i });
-  expect(btn).toBeEnabled();
-
-  const checkBox = screen.getByRole("checkbox");
-  expect(checkBox).not.toBeChecked();
-});
-
-it("should toggle button enabled/disabled", function () {
-  const btn = screen.getByRole("button", { name: /change to blue/i });
-  expect(btn).toBeEnabled();
-
-  const checkBox = screen.getByRole("checkbox", { name: "disabled button" });
-  expect(checkBox).not.toBeChecked();
-
-  fireEvent.click(checkBox);
-
-  expect(btn).toBeDisabled();
-
-  expect(btn).toHaveStyle({ backgroundColor: "grey" });
-
-  fireEvent.click(checkBox);
-
-  expect(btn).toBeEnabled();
-});
-
-it("should change btn color to grey when disabled", function () {
-  const btn = screen.getByRole("button", { name: /change to blue/i });
-  const checkbox = screen.getByRole("checkbox", { name: "disabled button" });
-
-  fireEvent.click(checkbox);
-  expect(btn).toHaveStyle({ backgroundColor: "grey" });
-
-  fireEvent.click(checkbox);
-  expect(btn).toHaveStyle({ backgroundColor: "red" });
-});
-
-describe("spaces before cames case", () => {
-  it("should works for no inner capital letters", function () {
-    expect(replaceCamelCase("Red")).toBe("Red");
-  });
-
-  it("should works for one inner capital letters", function () {
-    expect(replaceCamelCase("MidnightBlue")).toBe("Midnight Blue");
-  });
-
-  it("should works for multiple inner capital letters", function () {
-    expect(replaceCamelCase("MediumVioletRed")).toBe("Medium Violet Red");
-  });
-});
+// it("should have correct initial color", function () {
+//   const btn = screen.getByRole("button", { name: /change to blue/i });
+//
+//   expect(btn).toHaveStyle({ backgroundColor: "red" });
+//
+//   fireEvent.click(btn);
+//
+//   expect(btn).toHaveStyle({ backgroundColor: "blue" });
+//   expect(btn.textContent).toBe("Change to blue");
+// });
+//
+// it("should button be enabled", function () {
+//   const btn = screen.getByRole("button", { name: /change to blue/i });
+//   expect(btn).toBeEnabled();
+//
+//   const checkBox = screen.getByRole("checkbox");
+//   expect(checkBox).not.toBeChecked();
+// });
+//
+// it("should toggle button enabled/disabled", function () {
+//   const btn = screen.getByRole("button", { name: /change to blue/i });
+//   expect(btn).toBeEnabled();
+//
+//   const checkBox = screen.getByRole("checkbox", { name: "disabled button" });
+//   expect(checkBox).not.toBeChecked();
+//
+//   fireEvent.click(checkBox);
+//
+//   expect(btn).toBeDisabled();
+//
+//   expect(btn).toHaveStyle({ backgroundColor: "grey" });
+//
+//   fireEvent.click(checkBox);
+//
+//   expect(btn).toBeEnabled();
+// });
+//
+// it("should change btn color to grey when disabled", function () {
+//   const btn = screen.getByRole("button", { name: /change to blue/i });
+//   const checkbox = screen.getByRole("checkbox", { name: "disabled button" });
+//
+//   fireEvent.click(checkbox);
+//   expect(btn).toHaveStyle({ backgroundColor: "grey" });
+//
+//   fireEvent.click(checkbox);
+//   expect(btn).toHaveStyle({ backgroundColor: "red" });
+// });
+//
+// describe("spaces before cames case", () => {
+//   it("should works for no inner capital letters", function () {
+//     expect(replaceCamelCase("Red")).toBe("Red");
+//   });
+//
+//   it("should works for one inner capital letters", function () {
+//     expect(replaceCamelCase("MidnightBlue")).toBe("Midnight Blue");
+//   });
+//
+//   it("should works for multiple inner capital letters", function () {
+//     expect(replaceCamelCase("MediumVioletRed")).toBe("Medium Violet Red");
+//   });
+// });

35c14299a232dcb18616766cde44ebc281d04960
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-04-16 14:04:04 -0600

    order summary page

---
+++ src/pages/summary/OrderSummary.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useOrderDetails } from "../../context/orderDetails";
+import SummaryForm from "./SummaryForm";
+
+const OrderSummary = ({ setOrderPhase }) => {
+  const [orderDetails] = useOrderDetails();
+  const scoopArr = Array.from(orderDetails.scoops.entries());
+  const scoopList = scoopArr.map(([key, value]) => (
+    <li key={key}>
+      {value} {key}
+    </li>
+  ));
+
+  const toppingArr = Array.from(orderDetails.toppings.keys());
+  const toppingList = scoopArr.map((key) => <li key={key}>{key}</li>);
+  return (
+    <div>
+      <h1>Order Summary</h1>
+      <h2>Scoops: {orderDetails.total.scoops}</h2>
+      <ul>{scoopList}</ul>
+      <h2>Toppings {orderDetails.total.toppings}</h2>
+      <ul>{toppingList}</ul>
+      <SummaryForm setOrderPhase={setOrderPhase} />
+    </div>
+  );
+};
+
+export default OrderSummary;
